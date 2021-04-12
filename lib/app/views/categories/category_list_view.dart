6639e02ae603761158e60ad779b0c81bb56b63f4
İbrahim YILDIZ <yildizib@gmail.com>
2021-04-13 02:16:08 +0300

    ...

--- lib/app/views/categories/category_list_view.dart
+++ lib/app/views/categories/category_list_view.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:get_server/get_server.dart';
 import 'package:news_app_server/app/controllers/category_controller.dart';
 import 'package:news_app_server/app/models/category_model.dart';
-import 'package:news_app_server/app/utils/json.dart';
+import 'package:news_app_server/app/utils/json_util.dart';
 
 class CategoryListView extends GetView<CategoryController> {
   @override
